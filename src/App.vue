a40f9c79514e830f3c889cd0c78eccb4aa4550e8
armanjiwaji <armanjiwaji@gmail.com>
2021-10-04 15:29:40 +0530

    initial commit(Api)

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Api />
+  </div>
+</template>
+
+<script>
+import Api from "./components/Api.vue";
+
+export default {
+  name: "app",
+  components: {
+    Api,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Api.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Api Data</h1>
+    <!-- <button @click="getposts">get</button> -->
+
+    <div v-for="post in posts" :key="post.id">
+      <p>{{ post.id }}, {{ post.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Api",
+
+  created() {
+      this.getposts()
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    getposts() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => {
+          console.log(res.data);
+          this.posts = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
